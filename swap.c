4ac0fa0db8792e1c42a79ff9d8e256c2cb5f6098
Earl Francis Valdehuesa <efvaldehuesa@gmail.com>
2018-12-16 11:45:47 +0800

    Added new lines

--- swap.c
+++ swap.c
@@ -3,11 +3,11 @@
 int main() {
   int x = 35;
   int y = 42;
-  printf("x is %d and y is %d", x, y);
-  
+  printf("x is %d and y is %d\n", x, y);
+
   int temp = x;
   x = y;
   y = temp;
-  
-  printf("x is %d and y is %d", x, y);
+
+  printf("x is %d and y is %d\n", x, y);
 }
